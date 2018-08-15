00a0536d3885fe282075a037fbbec54ab33ede00
Ricky Kung <rickykung@me.com>
2018-08-15 16:39:31 +0800

    Add verbose-http-errors to nodeos startup command

--- eosio_docker/scripts/init_blockchain.sh
+++ eosio_docker/scripts/init_blockchain.sh
@@ -10,7 +10,7 @@ set -m
 
 # start nodeos ( local node of blockchain )
 # run it in a background job such that docker run could continue
-nodeos -e -p eosio -d /mnt/dev/data --config-dir /mnt/dev/config --http-validate-host=false --plugin eosio::wallet_api_plugin --plugin eosio::wallet_plugin --plugin eosio::producer_plugin --plugin eosio::history_plugin --plugin eosio::chain_api_plugin --plugin eosio::history_api_plugin --plugin eosio::http_plugin --http-server-address=0.0.0.0:8888 --access-control-allow-origin=* --contracts-console &
+nodeos -e -p eosio -d /mnt/dev/data --config-dir /mnt/dev/config --http-validate-host=false --plugin eosio::wallet_api_plugin --plugin eosio::wallet_plugin --plugin eosio::producer_plugin --plugin eosio::history_plugin --plugin eosio::chain_api_plugin --plugin eosio::history_api_plugin --plugin eosio::http_plugin --http-server-address=0.0.0.0:8888 --access-control-allow-origin=* --contracts-console --verbose-http-errors &
 sleep 1s
   until curl localhost:8888/v1/chain/get_info
 do
