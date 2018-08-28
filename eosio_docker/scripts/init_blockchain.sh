62b6f90df7847797fd7eb82f375ce67b5cc9d2d4
Ricky Kung <rickykung@me.com>
2018-08-28 10:26:30 +0800

    Update eosio/eos-dev docker image to v1.2.2

--- eosio_docker/scripts/init_blockchain.sh
+++ eosio_docker/scripts/init_blockchain.sh
@@ -32,12 +32,12 @@ done
 sleep 2s
 echo "=== setup wallet: eosiomain ==="
 # First key import is for eosio system account
-cleos wallet create -n eosiomain | tail -1 | sed -e 's/^"//' -e 's/"$//' > eosiomain_wallet_password.txt
+cleos wallet create -n eosiomain --to-console | tail -1 | sed -e 's/^"//' -e 's/"$//' > eosiomain_wallet_password.txt
 cleos wallet import -n eosiomain --private-key 5KQwrPbwdL6PhXujxW37FSSQZ1JiwsST4cqQzDeyXtP79zkvFD3
 
 echo "=== setup wallet: notechainwal ==="
 # key for eosio account and export the generated password to a file for unlocking wallet later
-cleos wallet create -n notechainwal | tail -1 | sed -e 's/^"//' -e 's/"$//' > notechain_wallet_password.txt
+cleos wallet create -n notechainwal --to-console | tail -1 | sed -e 's/^"//' -e 's/"$//' > notechain_wallet_password.txt
 # Owner key for notechainwal wallet
 cleos wallet import -n notechainwal --private-key 5JpWT4ehouB2FF9aCfdfnZ5AwbQbTtHBAwebRXt94FmjyhXwL4K
 # Active key for notechainwal wallet
