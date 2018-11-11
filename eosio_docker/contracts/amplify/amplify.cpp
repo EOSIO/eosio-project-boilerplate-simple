e065202ef468e72507dfefa873237f1656c969fb
Deon <deonsummers91@gmail.com>
2018-11-10 19:18:54 -0600

    listening to tokens

--- eosio_docker/contracts/amplify/amplify.cpp
+++ eosio_docker/contracts/amplify/amplify.cpp
@@ -3,6 +3,7 @@
 
 using namespace eosio;
 
+const name eostoken = "eosio.token"_n;
 
 CONTRACT amplify : public contract {
   //private:
@@ -11,14 +12,14 @@ CONTRACT amplify : public contract {
     amplify( eosio::name receiver, eosio::name code, eosio::datastream<const char*> ds ): eosio::contract(receiver, code, ds),  
       _proposal(receiver, code.value), 
       _users(receiver, code.value)
-    {}
+    {
+    }
 
     TABLE proposal {
       uint64_t      prim_key;  // primary key
       std::string name; // name of the proposal
       uint128_t users;  // number of users
       auto primary_key() const { return prim_key; }
-
     };
     
     typedef eosio::multi_index<"proposal"_n, proposal> proposaltable;
@@ -27,16 +28,19 @@ CONTRACT amplify : public contract {
 
     TABLE user {
       name user;
-      uint64_t last_stake; // date user last staked tokens
       bool active; //whether user is active or not
       uint128_t stake; // amount of tokens staked
       
       auto primary_key() const { return user.value; }
     };
 
+    
     typedef eosio::multi_index<"users"_n, user> usertable;
     usertable _users;
 
+    ACTION handledep() {
+
+    }
 
     ACTION stake(name user) { 
       require_auth(user);
@@ -45,9 +49,25 @@ CONTRACT amplify : public contract {
         new_user.active = true;
         new_user.user = user;
         new_user.stake = 5;
-        new_user.last_stake = now;
       });
+    }
+
+};
+
+
+extern "C" {
+    void apply(uint64_t receiver, uint64_t code, uint64_t action) {
+        if(code==receiver)
+        {
+            switch(action)
+            {
+              EOSIO_DISPATCH_HELPER( amplify, (stake) )
+            }
+        }
+        else if(code==eostoken.value && action=="transfer"_n.value) {
+            execute_action( name(receiver), name(code), &amplify::handledep );
+        }
     }
 };
 
-EOSIO_DISPATCH( amplify, (stake) )
+
